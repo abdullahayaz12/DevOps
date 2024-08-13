465dec91e7e6626dc7e92aeedfe0589a26eee0c2
Hafiz Abdullah Ayaz <76528924+abdullahayaz12@users.noreply.github.com>
2024-08-13 15:10:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World it is a new build '
+    return 'Hello World it is a new build, did it work? '
 
 @app.errorhandler(500)
 def server_error(e):
